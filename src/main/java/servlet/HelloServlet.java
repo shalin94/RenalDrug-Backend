f1b3f2fbd3aa9a022c8905c98e5d058597d72609
shalin94 <shalin94@gmail.com>
2013-07-15 00:05:51 -0700

    added tomcat to make it independent of heroku, added build.xml to enable running ant

---
+++ src/main/java/servlet/HelloServlet.java
@@ -0,0 +1,33 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Test
+ */
+@WebServlet(name = "HelloServlet", urlPatterns = {"/"})
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		ServletOutputStream out = res.getOutputStream();
+		out.write("hello dude!".getBytes());
+		out.flush();
+		out.close();
+		
+	}
+
+}
